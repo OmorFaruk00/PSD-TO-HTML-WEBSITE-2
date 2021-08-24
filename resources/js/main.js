d17e6c3de58e99921018a162c52cb3e0d485ca70
Omor Faruk <omorfaruk5020@gmail.com>
2021-08-25 00:01:53 +0600

    add file

---
+++ resources/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	var mixer = mixitup(".container");
+
+});
+$(document).ready(function(){
+
+	$(".js-services-section").waypoint(function(direction){
+		if(direction == "down"){
+			$("nav").addClass("sticky");
+		}else{
+			$("nav").removeClass("sticky");
+		}
+	});
+
+	
+	$("a").on("click", function(event){
+		if(this.hash !== ""){
+			event.preventDefault();
+			var hash = this.hash;
+			$("html,body").animate({
+				scrollTop:$(hash).offset().top 
+			},800,function(){
+				window.location.hash = hash;
+			});
+		}
+
+	});
+
+});
+	function openNav(){
+		document.getElementById("myNav").style.width="100%";
+	}
+	function closeNav(){
+		document.getElementById("myNav").style.width="0%";
+	}
